daca29e2272c0ee8911425bb7ca43abdd338eb58
Jorge <jorjoluiso@gmail.com>
2022-10-19 20:25:27 -0500

    [Added] Example of the chapter 3

---
+++ doc/examples/chapter3/library_module/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "library_module",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Esta es una descripcion extendidad
+        de mi nuevo modulo
+        para aprender odoo
+    """,
+
+    'author': "Jorge Luis",
+    'website': "http://mestizos.dev",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Services/Library',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/book_view.xml',
+        'views/library_menu.xml',
+        'views/book_list_template.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'application': True
+}
